13f18d58bfb2916653dca0a39b0cca6db69afcd3
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2023-07-28 22:43:28 +0430

    Add Production Build

--- src/components/Footer/Subtitle.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Subtitle.css"
-
-
-export default function Subtitle({innerText, href}){
-    return(
-        <a href={href} className="footer-subtitle">{innerText}</a>
-    )
-}
--- src/components/Footer/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./Title.css"
-
-
-export default function Title({innerText}){
-    return(
-        <h5 className="footer-title">{innerText}</h5>
-    )
-}
